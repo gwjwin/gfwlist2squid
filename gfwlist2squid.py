d3f9180f542238ae5bffc6d2f3100331983623f5
gwjwin <gwjwin@sina.com>
2016-01-05 09:40:19 +0800

    Update gfwlist2squid.py

--- gfwlist2squid.py
+++ gfwlist2squid.py
@@ -6,16 +6,8 @@
 # gwflist | Adblock Format 
 # https://adblockplus.org/en/filters
 
-# squid rules
-# squid only matchs DOMAIN, not URL, so the rules in gwflist should changed to DOMAIN.
-
-# Example:
-# china-mmm.jp.net                      -> china\-mmm\.jp\.net
-# ||china-mmm.net                       -> ^china\-mmm\.net
-# |http://85.17.73.31/                  -> ^85\.17\.73\.31
-# 21andy.com/blog                       -> 21andy\.com
-# |http://cdn*.abc.com/                 -> ^cdn(.*)\.abc\.com
-# /^https?:\/\/[^\/]+blogspot\.(.*)/    -> ^blogspot\.(.*)
+# Squid 'acl dstdom_regex' Rules
+# dstdom_regex only matchs DOMAIN, not URL, so the rules in gwflist should changed to DOMAIN.
 
 # Usage:
 # Add following lines into squid.conf: (squid v2.7)
@@ -35,86 +27,106 @@
 # via off
 # ----
 
+# Example:
+# china-mmm.jp.net                      -> china\-mmm\.jp\.net
+# ||china-mmm.net                       -> ^china\-mmm\.net
+# |http://85.17.73.31/                  -> ^85\.17\.73\.31$
+# 21andy.com/blog                       -> 21andy\.com$
+# |http://cdn*.abc.com/                 -> ^cdn(.*)\.abc\.com
+# /^https?:\/\/[^\/]+blogspot\.(.*)/    -> ^blogspot\.(.*)
+# @@||baidu.com                         -> ^baidu\.com
+
 import urllib2
 import re
 from base64 import b64decode
 
 
-LIST_URL   = 'https://raw.githubusercontent.com/gfwlist/gfwlist/master/gfwlist.txt'
+GWFLIST_URL   = 'https://raw.githubusercontent.com/gfwlist/gfwlist/master/gfwlist.txt'
+GWFLIST_FILE  = 'gwflist.txt'
 DECODE_FILE  = 'gwflist.decode.txt'
 WHITE_FILE = 'whitelist.url_regex.lst'
 BLACK_FILE = 'balcklist.url_regex.lst'
 
+def url2domain(s):
+  #remove http(s)://
+  if (s.startswith('http://')):
+    s = s[7:]
+  if (s.startswith('https://')):
+    s = s[8:]
+  #remove url path
+  i = s.find('/')
+  if (i >= 0){
+    s = s[:i]
+  }
+  #escape regex chars
+  r = re.escape(s)
+  r = r.replace(r'\*', '(.*)')
+  if (r.endswith(r'\/'))
+    r = r[:-2] + '$'
+  return r
+
 def convert_line(line):
-    line = line.rstrip()
-    #regex already
-    if line[0] == '/' and line[-1] == '/':
-        #remove https?:\/\/[^\/]+
-        rline = line[1:-1]
-        rline = rline.replace(r'^https?:\/\/[^\/]+', r'^[^\/]+')
-        return rline
+  line = line.rstrip()
+  
+  #regex rules
+  if line[0] == '/' and line[-1] == '/':
+    #simple remove https?:\/\/[^\/]+
+    rline = line[1:-1]
+    rline = rline.replace(r'https?:\/\/[^\/]+', '')
+    return rline
+  
+  #normal rules
+  if line.startswith('||'):
+    #add ^ at beginning    
+    rline = line[2:]
+    rline = url2domain(rline)
+    return '^' + rline
+  elif line.startswith('|'):
+    rline = line[1:]
+    rline = url2domain(rline)
+    return '^' + rline
+  elif line[-1] == '|':
+    rline = line[:-1]
+    rline = url2domain(rline)
+    return rline + '$'
+  else:
+    rline = line
+    rline = url2domain(rline)
+    return rline
     
-    if line.startswith('||'):
-        rline = line[2:]
-        rline = rline.replace(r'http://', '')
-        rline = rline.replace(r'https://', '')
-        rline = re.escape(rline)
-        rline = rline.replace(r'\*', '(.*)')
-        #return '^https?:\/\/[^\/]+' + rline
-        return '^[^\/]*' + rline
-    elif line.startswith('|'):
-        rline = line[1:]
-        rline = rline.replace(r'http://', '')
-        rline = rline.replace(r'https://', '')
-        rline = re.escape(rline)
-        rline = rline.replace(r'\*', '.*')
-        return '^' + rline
-    elif line[-1] == '|':
-        rline = line[:-1]
-        rline = rline.replace(r'http://', '')
-        rline = rline.replace(r'https://', '')
-        rline = re.escape(rline)
-        rline = rline.replace(r'\*', '.*')
-        return rline + '$'
-    else:
-        rline = line
-        rline = rline.replace(r'http://', '')
-        rline = rline.replace(r'https://', '')
-        rline = re.escape(rline)
-        rline = rline.replace(r'\*', '.*')
-        return rline
 
         
 def convert(gfwlist):
-    black = open(BLACK_FILE, 'w')
-    white = open(WHITE_FILE, 'w')
-    
-    for l in gfwlist.split('\n'):
-        #l = l[:-1]
-        if not l or l[0] == '!' or l[0] == '[':
-            continue
-            
-        if l.startswith('@@'):
-            white.write(convert_line(l[2:]) + '\n')
-        else:
-            black.write(convert_line(l) + '\n')
+  black = open(BLACK_FILE, 'w')
+  white = open(WHITE_FILE, 'w')
+  
+  for l in gfwlist.split('\n'):
+    if not l or l[0] == '!' or l[0] == '[':
+      continue
+        
+    if l.startswith('@@'):
+      white.write(convert_line(l[2:]) + '\n')
+    else:
+      black.write(convert_line(l) + '\n')
 
             
 def main():
 
+  #set read gwflist from URL or FILE
   isFromURL = 1
   
   if (isFromURL):
-    #open from URL
-    src = urllib2.urlopen(LIST_URL).read()
-    src = b64decode(src)
-    decode = open(DECODE_FILE, 'w')
-    decode.write(src)
-    convert(src)
+    #read from URL
+    src = urllib2.urlopen(GWFLIST_URL).read()
   else:
+    #read from FILE
     open from local file
-    src = open(DECODE_FILE, 'r').read()
-    convert(src)
+    src = open(GWFLIST_FILE, 'r').read()
+
+  src = b64decode(src)
+  decode = open(DECODE_FILE, 'w')
+  decode.write(src)
+  convert(src)
              
 if __name__ == '__main__':
   main()
