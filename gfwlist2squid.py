8668680b52a2dcf97142237e596d9853dc179177
gwjwin <gwjwin@sina.com>
2016-01-05 10:09:15 +0800

    Initial commit

--- gfwlist2squid.py
+++ gfwlist2squid.py
@@ -3,26 +3,26 @@
 
 # forked from sorz/gfwlist2regex.py
 
-# gwflist | Adblock Format 
+# gfwlist | Adblock Format 
 # https://adblockplus.org/en/filters
 
 # Squid 'acl dstdom_regex' Rules
-# dstdom_regex only matchs DOMAIN, not URL, so the rules in gwflist should changed to DOMAIN.
+# dstdom_regex only matchs DOMAIN, not URL, so the rules in gfwlist should changed to DOMAIN.
 
 # Usage:
 # Add following lines into squid.conf: (squid v2.7)
 # ----
 # acl whitelist dstdom_regex 'whitelist.url_regex.lst'
-# acl balcklist dstdom_regex 'balcklist.url_regex.lst'
+# acl blacklist dstdom_regex 'blacklist.url_regex.lst'
 # prefer_direct on
 # always_direct allow whitelist
-# never_direct  allow balcklist
+# never_direct  allow blacklist
 # cache_peer 127.0.0.1 parent 48081 0 name=ss1 round-robin
 # cache_peer 127.0.0.1 parent 48082 0 name=ss2 round-robin
 # cache_peer_access ss1 deny whitelist
 # cache_peer_access ss2 deny whitelist
-# cache_peer_access ss1 allow balcklist
-# cache_peer_access ss1 allow balcklist
+# cache_peer_access ss1 allow blacklist
+# cache_peer_access ss1 allow blacklist
 # forwarded_for transparent
 # via off
 # ----
@@ -41,11 +41,11 @@ import re
 from base64 import b64decode
 
 
-GWFLIST_URL   = 'https://raw.githubusercontent.com/gfwlist/gfwlist/master/gfwlist.txt'
-GWFLIST_FILE  = 'gwflist.txt'
-DECODE_FILE  = 'gwflist.decode.txt'
+GFWLIST_URL   = 'https://raw.githubusercontent.com/gfwlist/gfwlist/master/gfwlist.txt'
+GFWLIST_FILE  = 'gfwlist.txt'
+DECODE_FILE  = 'gfwlist.decode.txt'
 WHITE_FILE = 'whitelist.url_regex.lst'
-BLACK_FILE = 'balcklist.url_regex.lst'
+BLACK_FILE = 'blacklist.url_regex.lst'
 
 def url2domain(s):
   #remove http(s)://
@@ -111,19 +111,19 @@ def convert(gfwlist):
             
 def main():
 
-  #set read gwflist from 0=URL or 1=FILE or 2=Plain-text FILE
+  #read gfwlist from 0=URL or 1=FILE or 2=Plain-text FILE
   rMode = 0
   
   if rMode == 0:
     #read from URL
-    src = urllib2.urlopen(GWFLIST_URL).read()
+    src = urllib2.urlopen(GFWLIST_URL).read()
     src = b64decode(src)
     decode = open(DECODE_FILE, 'w')
     decode.write(src)
     convert(src)
   elif rMode == 1:
     #read from FILE
-    src = open(GWFLIST_FILE, 'r').read()
+    src = open(GFWLIST_FILE, 'r').read()
     src = b64decode(src)
     decode = open(DECODE_FILE, 'w')
     decode.write(src)
